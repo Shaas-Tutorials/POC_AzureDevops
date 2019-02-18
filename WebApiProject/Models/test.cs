a70571d11d357cd2566d2f169c34f6de47612b8c
Kartik Sharma <kartiksharma7@deloitte.com>
2019-02-18 21:07:43 +0530

    deleted test.cs

--- WebApiProject/Models/test.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApiProject.Models
-{
-    public class Employee : IEmployee
-    {
-        public int EmployeeId { get; set ; }
-        public string EmployeeName { get; set; }
-        public string Department { get; set; }
-    }
-}
